e2281398ee29360ff8f8273df67c8f820ac40e23
xifeng jin <xifeng.jin88@gmail.com>
2024-05-16 11:02:02 -0500

    different direction

---
+++ src/sidepanel.tsx
@@ -0,0 +1,11 @@
+import "~style.css"
+
+function IndexSidepanel() {
+  return (
+    <div className="w-full p-2 flex items-center justify-center">
+      <h1>welcome</h1>
+    </div>
+  )
+}
+
+export default IndexSidepanel
