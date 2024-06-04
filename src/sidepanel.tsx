fb8f546ab9798923b9a05b0cee61980d59a802e1
xifeng jin <xifeng.jin88@gmail.com>
2024-06-03 23:44:03 -0500

    add components for forms

--- src/sidepanel.tsx
+++ src/sidepanel.tsx
@@ -1,12 +1,57 @@
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { useStorage } from "@plasmohq/storage/hook"
+
 import { Button } from "~components/ui/Button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from "~components/ui/dialog"
 
 import "~style.css"
 
+const TIME_STAMP_REGEX = /^(?:(?:(\d+):)?(\d+):)?(\d+)$/
+
+const timestampFormSchema = z.object({
+  timeStamp: z
+    .string()
+    .min(1, {
+      message: "Timestamp cannot be empty"
+    })
+    .regex(TIME_STAMP_REGEX, {
+      message: "Please enter a correct timestamp"
+    })
+})
+
 function IndexSidepanel() {
+  const [timestamps, setTimestamps] = useStorage("timestamp", [])
+  const form = useForm()
+
+  function onSubmit(values: z.infer<typeof timestampFormSchema>) {
+    console.log(values)
+  }
+
   return (
     <div className="w-full p-2 flex items-center justify-center">
       <div className="my-4">
-        <Button>Add Timestamp</Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="ghost">Add timestamp</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>And a new timestamp</DialogTitle>
+              <DialogDescription>
+                Save timestamp and note associated with it.
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
       </div>
     </div>
   )
